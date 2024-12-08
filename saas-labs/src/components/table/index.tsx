df7cbd405443f092d8de6aaf3d88525aaf7f9d50
Rohan Gupta <rohan.gupta@zopper.com>
2024-12-08 23:32:04 +0530

    refactor: unused imports commneted

--- saas-labs/src/components/table/index.tsx
+++ saas-labs/src/components/table/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+// import React from "react";
 import { IProjectsStatsItem } from "../../models";
-// import styles from "./index.module.css";
-const styles = {
-  loadingCell: "",
-};
+import styles from "./index.module.css";
+// const styles = {
+//   loadingCell: "",
+// };
 
 interface ITableProps {
   tableData: IProjectsStatsItem[];
